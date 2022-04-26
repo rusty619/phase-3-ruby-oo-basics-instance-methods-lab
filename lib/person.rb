401028a1420a2ddbcdf5ba4af53b3affa900d2a4
Ruslan Khait <rusty6916@gmail.com>
2022-04-26 10:39:42 -0400

    commit tree 4a99aed79191ffe0527cc09f49ee6a2f4eededec

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+ruslan = Person.new
+
+puts ruslan.talk
+puts ruslan.walk
